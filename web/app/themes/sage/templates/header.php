63ed3eae1b9adfb0796f0f08c5378a9321594ae8
Thomas Azar <design@thomasazar.net>
2016-08-15 16:46:09 -0400

    Old styles for nav

--- web/app/themes/sage/templates/header.php
+++ web/app/themes/sage/templates/header.php
@@ -1,10 +1,10 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <nav class="nav nav--main">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list nav__list nav__list--main']);
       endif;
       ?>
     </nav>
