2cf4a6d4aa2b88ca063ce75b6b7cf0c9b0bbb97e
Thomas Azar <design@thomasazar.net>
2016-08-15 16:48:03 -0400

    Header

--- web/app/themes/sage/templates/header.php
+++ web/app/themes/sage/templates/header.php
@@ -1,12 +1,13 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav nav--main">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list nav__list nav__list--main']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header class="header header--top header--front-page">
+  <h1 class="header__text header__text--top">
+    <a href="<?= esc_url(home_url('/')); ?>" title="Return to home" class="header__link header__link--top"><?php bloginfo('name'); ?></a>
+  </h1>
+  <h2 class="header__subtext header__subtext--top"><?php bloginfo('description'); ?></h2>
+  <nav class="nav nav--main">
+    <?php
+    if (has_nav_menu('primary_navigation')) :
+      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'list nav__list nav__list--main']);
+    endif;
+    ?>
+  </nav>
 </header>
