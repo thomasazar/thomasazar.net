522b4686f6d8153ce6b796ba03a2c06ba6c0683f
Thomas Azar <design@thomasazar.net>
2016-08-15 10:15:54 -0400

    Initial commit

---
+++ config/environments/development.php
@@ -0,0 +1,7 @@
+<?php
+@ini_set('display_errors','On'); // enable or disable public display of errors (use 'On' or 'Off')
+
+define('SAVEQUERIES', true );
+define('WP_DEBUG', true );
+define('LOCALHOST', true);
+define('SCRIPT_DEBUG', true);
