77b62ffa3cac20586b95125fe947f59f21185828
Thomas Azar <design@thomasazar.net>
2016-09-20 15:49:24 -0400

    Design logo in footer

--- web/app/themes/sage/templates/footer.php
+++ web/app/themes/sage/templates/footer.php
@@ -1,5 +1,5 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="footer footer--main">
+	<a href="http://design.thomasazar.net" alt="Design by Thomas Azar">
+		<img src="<?= get_template_directory_uri(); ?>/dist/images/design-by-thomas-azar.png" alt="Design by Thomas Azar">
+	</a>
 </footer>
