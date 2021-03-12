083600b5f01a04088151cdcafb34eb1a18a446e8
Thomas Azar <design@thomasazar.net>
2021-03-12 09:13:47 -0500

    Use JSON for config. Write home page.

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ collections.forEach(collection => {
     .then(res => {
         if (collection !== 'home') {
             res.data.data.forEach(item => {
-                // Prep the file
-                const slug = slugify(item.title, {lower: true}); // Create slug based on the post's title
                 var location = file = 'content/';
-                if (item.location) {
-                    location += item.location + '/';
-                    file = location + slug + '.md';
-                    delete item.location;
+                
+                if (item.title === 'Home'){
+                    file = 'content/_index.md';
                 } else {
-                    location += slug;
-                    file = location + '/_index.md';
+                    // Prep the file
+                    const slug = slugify(item.title, {lower: true}); // Create slug based on the post's title
+                    if (item.location) {
+                        location += item.location + '/';
+                        file = location + slug + '.md';
+                        delete item.location;
+                    } else {
+                        location += slug;
+                        file = location + '/_index.md';
+                    }
                 }
 
                 // Create directory structure if needed
@@ -41,3 +46,25 @@ collections.forEach(collection => {
         console.error(err.message);
     })
 });
+
+// Fetch settings
+axios.get(`${directus}/settings?fields=*,menu.*`)
+.then(res => {
+    var item = res.data.data;
+    var settings = {};
+    settings.title = item.title;
+    settings.description = item.description;
+    delete item.title;
+    delete item.description;
+    settings.params = item;
+
+    // Write the file
+    fs.writeFile('config.json', JSON.stringify(settings, null, 2), (err) => {
+        if (err) { console.error(err); }
+        else { console.log('Wrote config.json'); }
+    });
+
+})
+.catch(err => {
+    console.error(err.message);
+})
